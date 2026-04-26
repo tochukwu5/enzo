051c4b827139ff2a410624b49014c2bb349ee7a0
kceegram <158181757+kceegram@users.noreply.github.com>
2026-04-26 05:40:29 +0100

    new commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',  // ← ADD THIS LINE
+  plugins: [
+    react(),
+  ],
 })
